989224e3eb12e73156aaea9fcee2c10242d4bfea
zhang <18971126088@163.com>
2021-08-26 09:33:52 +0800

    第一版本

---
+++ js/json.js
@@ -0,0 +1,13 @@
+let full ={
+    steps1:{
+        // 错别字
+        textEnd:['wodebaoya'],
+        //切换输入法
+        cutover:'zh',
+        // 输入文字
+        text:['"咳咳咳" \n zz \n xxx'],
+        
+    },
+    steps2:[],
+    steps3:[],
+}
